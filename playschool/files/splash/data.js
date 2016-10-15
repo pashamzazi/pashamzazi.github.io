615b58055f296a80aee65f2b0db7261f2bc0e103
Charles Wahome <cwahome@virtualcity.co.ke>
2016-10-15 09:48:56 +0300

    add playschool files

---
+++ playschool/files/splash/data.js
@@ -0,0 +1,183 @@
+﻿$axure.loadCurrentPage({
+  "url":"splash.html",
+  "generationDate":new Date(1476513100331.52),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"8cd9ebff7ea64af0b61a856ece28b525",
+    "type":"Axure:Page",
+    "name":"Splash",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"center",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"center",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+      "onLoad":{
+        "description":"OnLoad",
+        "cases":[{
+            "description":"Case 1",
+            "isNewIfGroup":false,
+            "actions":[{
+                "action":"wait",
+                "description":"Wait 3000 ms",
+                "waitTime":3000},
+{
+                "action":"linkWindow",
+                "description":"Open Login in Current Window",
+                "target":{
+                  "targetType":"page",
+                  "url":"login.html",
+                  "includeVariables":true},
+                "linkType":"current"}]}]}},
+    "diagram":{
+      "objects":[{
+          "id":"5b203ff6d61c4519b6b64dddff0bd21a",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":90,
+              "y":-43},
+            "size":{
+              "width":469,
+              "height":813}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"a76233c8c718411a903489da6140957e",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":90,
+                  "y":-43},
+                "size":{
+                  "width":469,
+                  "height":813}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/splash/u0.png"}},
+{
+          "id":"5940f3ebe2a442a59dd4136b1cd73097",
+          "label":"",
+          "type":"buttonShape",
+          "styleType":"buttonShape",
+          "visible":true,
+          "style":{
+            "fill":{
+              "fillType":"solid",
+              "color":0xFFF9FAF5},
+            "location":{
+              "x":181,
+              "y":103},
+            "size":{
+              "width":295,
+              "height":523},
+            "borderWidth":"-1",
+            "borderFill":{
+              "fillType":"solid",
+              "color":0xFFFFFF}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"b0d12751fc6c45359dc3ded03c631a0e",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "fill":{
+                  "fillType":"solid",
+                  "color":0xFFF9FAF5},
+                "location":{
+                  "x":181,
+                  "y":103},
+                "size":{
+                  "width":295,
+                  "height":523},
+                "borderWidth":"-1",
+                "borderFill":{
+                  "fillType":"solid",
+                  "color":0xFFFFFF}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/splash/u2.png"}},
+{
+          "id":"b897264056ee4b3e8ce71f93d1d08550",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "foreGroundFill":{
+              "fillType":"solid",
+              "color":0xFF8445AD,
+              "opacity":1},
+            "location":{
+              "x":187,
+              "y":210},
+            "size":{
+              "width":280,
+              "height":280}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"69402118812a4bd49e0e8ea76e25ebae",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "foreGroundFill":{
+                  "fillType":"solid",
+                  "color":0xFF8445AD,
+                  "opacity":1},
+                "location":{
+                  "x":187,
+                  "y":210},
+                "size":{
+                  "width":280,
+                  "height":280}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/login/u19.jpg"}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "5b203ff6d61c4519b6b64dddff0bd21a":{
+      "scriptId":"u0"},
+    "a76233c8c718411a903489da6140957e":{
+      "scriptId":"u1"},
+    "5940f3ebe2a442a59dd4136b1cd73097":{
+      "scriptId":"u2"},
+    "b0d12751fc6c45359dc3ded03c631a0e":{
+      "scriptId":"u3"},
+    "b897264056ee4b3e8ce71f93d1d08550":{
+      "scriptId":"u4"},
+    "69402118812a4bd49e0e8ea76e25ebae":{
+      "scriptId":"u5"}}});
