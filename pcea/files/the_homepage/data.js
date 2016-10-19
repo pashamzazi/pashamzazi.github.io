c9ae9935476e54d3b2b1e950c9739077285b016f
Charles Wahome <cwahome@virtualcity.co.ke>
2016-10-19 07:34:43 +0300

    add pcea files

---
+++ pcea/files/the_homepage/data.js
@@ -0,0 +1,79 @@
+﻿$axure.loadCurrentPage({
+  "url":"the_homepage.html",
+  "generationDate":new Date(1476851067762.39),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"e1410970e74e48cfbdb8fbf7d9859cc2",
+    "type":"Axure:Page",
+    "name":"The Homepage",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[{
+          "id":"271fde4181e8425dab8616d4f1ae6492",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "foreGroundFill":{
+              "fillType":"solid",
+              "color":0xFF333333,
+              "opacity":1},
+            "location":{
+              "x":26,
+              "y":0},
+            "size":{
+              "width":1024,
+              "height":6514}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"f28a0489819a4885a66ba7f73d81f4a8",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "foreGroundFill":{
+                  "fillType":"solid",
+                  "color":0xFF333333,
+                  "opacity":1},
+                "location":{
+                  "x":26,
+                  "y":0},
+                "size":{
+                  "width":1024,
+                  "height":6514}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/the_homepage/u0.jpg"}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "271fde4181e8425dab8616d4f1ae6492":{
+      "scriptId":"u0"},
+    "f28a0489819a4885a66ba7f73d81f4a8":{
+      "scriptId":"u1"}}});
